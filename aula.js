b7a59f98cd83bfd8804de02ecd50201f93768158
freitassp <priettosamuel@gmail.com>
2020-10-20 22:59:04 -0300

    curso javascript

---
+++ aula.js
@@ -0,0 +1,14 @@
+//let idade = 6;
+//console.log(idade);
+//let nome = 'samuel';
+//console.log(nome);
+
+//const valorIngressoAdulto = 20;
+//valorIngressoAdulto = 25;
+//console.log(valorIngressoAdulto);
+
+let nome = 'samuel';//string literal
+let idade = 30; //number literal
+let estaAprovado = true; //boolean
+let sobrenome; //undefined
+let corSelecionada =null; //
